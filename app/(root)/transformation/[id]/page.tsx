5e6a3e539f2da32ce1bf59d9f4999fdfdabefa5e
Aayush Singh <aayushsingh1239@gmail.com>
2025-05-02 11:56:30 +0545

    first commit

---
+++ app/(root)/transformation/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TransformationPage = () => {
+    return (
+        <div>
+            transformation
+        </div>
+    )
+}
+
+export default TransformationPage
